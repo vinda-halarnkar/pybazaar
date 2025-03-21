d3c663da837e31dbc5abb6753063b54ca14ff08d
Raina Gomes <gomesraina19@gmail.com>
2025-03-21 16:18:22 +0530

    added Black hook

--- static/bundles/main.js
+++ static/bundles/main.js
@@ -21,12 +21,12 @@ eval("\n\n//# sourceURL=webpack://pybazaar/./frontend/index.js?");
 
 /******/ 	});
 /************************************************************************/
-/******/ 	
+/******/
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./frontend/index.js"]();
-/******/ 	
+/******/
 /******/ })()
 ;
