be5bd054e93085b158643af639fb668dfc1656a8
Raina Gomes <gomesraina19@gmail.com>
2025-03-21 15:31:36 +0530

    Added Product models to Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from products.models import ProductImage, AvailabilityStatus, Brand, Category, Product
+
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(AvailabilityStatus)
+admin.site.register(Product)
+admin.site.register(ProductImage)
