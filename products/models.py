26d29d0c6942703864c4af2e24dc590dfacc9ba3
Raina Gomes <gomesraina19@gmail.com>
2025-03-21 10:48:04 +0530

    Added ptoduct import command

---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Category(models.Model):
+    category_slug = models.CharField(max_length=30, unique=True)
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
+
+
+class AvailabilityStatus(models.Model):
+    status_name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.status_name
+
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.brand_name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
+    price = models.FloatField()
+    discount_percentage = models.FloatField()
+    stock = models.IntegerField()
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, related_name='products')
+    sku = models.CharField(max_length=20, unique=True)
+    availability_status = models.ForeignKey(AvailabilityStatus, on_delete=models.SET_NULL, null=True,
+                                            related_name='products')
+
+    def __str__(self):
+        return self.title
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image_path = models.CharField(max_length=255, null=False)
+    is_thumbnail = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Image for {self.product.title}"
