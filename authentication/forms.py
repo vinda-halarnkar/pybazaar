152879ce0b22c24490a561d566d47a35474d1a62
Vinda <vinda@smartshore.nl>
2025-03-25 17:17:10 +0530

    Username changes

--- authentication/forms.py
+++ authentication/forms.py
@@ -10,13 +10,13 @@ class RegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=150)
     last_name = forms.CharField(max_length=150)
     email = forms.EmailField(validators=[validators.EmailValidator])
-    username = forms.CharField(max_length=150)
+    # username = forms.CharField(max_length=150)
     password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
     password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
     class Meta:
         model = UserProfile
-        fields = ["first_name", "last_name", "username", "email", "password1", "password2" ]
+        fields = ["first_name", "last_name", "email", "password1", "password2" ]
 
     def clean_email(self):
         """
--- authentication/models.py
+++ authentication/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class UserProfile(AbstractUser):
     email = models.EmailField(unique=True, null=False)
+    username = models.CharField(max_length=150, blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
