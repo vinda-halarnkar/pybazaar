2260994f57391efc2316a0ce20b3996e82ff8cfc
Raina Gomes <gomesraina19@gmail.com>
2025-03-28 16:36:54 +0530

    scripts setup

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('Your script has loaded!s');
+import "jquery";
+
+import "bootstrap";
+import "nouislider";
+import "magnific-popup";
+import "owl.carousel";
+import "gmaps";
+import "choices.js";
+
+// Import additional jQuery plugins
+import "./theme/jquery.ajaxchimp.min.js";
+import "./theme/jquery.sticky.js";
+import "./theme/countdown.js";
+
+// Import Theme Js
+import "./theme.js";
+
+// Load Google Maps API dynamically
+// const googleMapsScript = document.createElement("script");
+// googleMapsScript.src =
+//   "https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE";
+// googleMapsScript.async = true;
+// googleMapsScript.defer = true;
+// document.head.appendChild(googleMapsScript);
+
+console.log('Your script has loaded!');
+console.log("jQuery Version:", $.fn.jquery);
