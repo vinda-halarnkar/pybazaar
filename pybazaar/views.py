998c06a8885d3e516a8792ef2fcabc97996a2d35
vinda <vinda.halarnkar@gmail.com>
2025-04-04 17:15:50 +0530

    Pre commit

--- pybazaar/views.py
+++ pybazaar/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import DatabaseError
+from products.models import Category, Product
+from django.db.models import Min
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @login_required
@@ -9,7 +15,23 @@ def index(request):
 
 
 def home(request):
-    return render(request, "home.html")
+    try:
+        products = (
+            Product.objects.filter(images__is_thumbnail=False)
+            .order_by("-created_at")
+            .values("id", "title", "description")
+            .annotate(image_path=Min("images__image_path"))[:3]
+        )
+
+        categories = Category.objects.values("category_name")[:5]
+
+    except DatabaseError as e:
+        logger.error(f"Database error: {e}")
+        products = []
+        categories = []
+
+    context = {"latest_products": products, "categories": list(categories)}
+    return render(request, "home.html", context)
 
 
 def contact(request):
